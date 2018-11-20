f3e9750d140872c1257ff4f3a31329d417bc6248
Sam Bennrubi <sbennrubi@gmail.com>
2018-11-20 00:39:09 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -3,7 +3,7 @@ def reformat_languages(languages)
 new_hash = {}
   languages.each do |use, language_list|
     language_list.map do |language, description|
-    new_hash[language][:type] = description.value
+    new_hash[language][:type] = description.val
     if new_hash[language][:style] == nil
       new_hash[language][:style] = [use]
     else 
